f08ffb512082dbb3fd02f6bf2b0e48781fb80a87
lolgeny <joel@swedensky.com>
2021-03-02 17:19:35 +0000

    Modified dependencies + hello, server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+const express = require('express');
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+});
